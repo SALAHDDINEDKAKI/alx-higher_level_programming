9c15ff020c1a85cd6a7e79fdf50f15a111ec1fde
SALAHDDINEDKAKI <salahddine.dkaki@edu.uiz.ac.ma>
2023-12-03 16:46:25 -0800

    task0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,6 @@
 
 def print_list_integer(my_list=[]):
     for integer in my_list:
-        print("{}".format(str(integer)))
+        print("{}".format(integer))
 my_list = ["1", "2", "3", "4", "5"]
 print_list_integer(my_list)
