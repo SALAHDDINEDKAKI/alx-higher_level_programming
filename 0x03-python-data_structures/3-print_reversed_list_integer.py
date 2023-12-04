56f7464f6b9a77df6d10088108f45d43350e5c95
SALAHDDINEDKAKI <salahddine.dkaki@edu.uiz.ac.ma>
2023-12-03 18:12:58 -0800

    task3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if ininstance(my_list, list):
+    if isinstance(my_list, list):
         mt_list.reverse()
         for inreger in my_list:
             print("{:d}".format(integer))
