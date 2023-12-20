61b01a2e8525dc389d1b02430d72d123356d5b2f
SALAHDDINEDKAKI <salahddine.dkaki@edu.uiz.ac.ma>
2023-12-20 08:07:04 -0800

    tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""an empty class square that defines a square"""
+class Square:
+    """ represents a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Square defines a square"""
+
+class Square:
+    """
+    Represents a square.
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        iinitializes a square instance.
+
+        aargs:
+            size (int): The size of the square.
+        """
+        self.__size = size
